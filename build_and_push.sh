9a43f3fdebb73877cf46393d23715643c73e029e
pekachoo <jliu6162@gmail.com>
2026-01-15 13:19:40 -0500

    most recent build setup

--- build_and_push.sh
+++ build_and_push.sh
@@ -16,23 +16,23 @@ fi
 echo "Syncing bb_isaaclab_ws..."
 "$SYNC_SCRIPT"
 
-# --- Ensure conda is available in non-interactive shell ---
-if [[ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]]; then
-  source "$HOME/miniconda3/etc/profile.d/conda.sh"
-elif [[ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]]; then
-  source "$HOME/anaconda3/etc/profile.d/conda.sh"
-else
-  echo "Error: conda.sh not found. Is Conda installed?"
-  exit 1
-fi
+# # --- Ensure conda is available in non-interactive shell ---
+# if [[ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]]; then
+#   source "$HOME/miniconda3/etc/profile.d/conda.sh"
+# elif [[ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]]; then
+#   source "$HOME/anaconda3/etc/profile.d/conda.sh"
+# else
+#   echo "Error: conda.sh not found. Is Conda installed?"
+#   exit 1
+# fi
 
-# --- Activate env ---
-echo "Activating conda environment: $CONDA_ENV"
-conda activate "$CONDA_ENV"
+# # --- Activate env ---
+# echo "Activating conda environment: $CONDA_ENV"
+# conda activate "$CONDA_ENV"
 
 # --- Build image ---
 echo "Running Docker build script..."
-python ./docker/container.py
+python ./docker/container.py build
 
 # --- Tag image ---
 echo "Tagging image..."
